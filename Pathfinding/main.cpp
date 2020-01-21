500a18a2c7144679e766440f027ec31fd7db68b2
Ben16 <f.benjamin2020@gmail.com>
2020-01-20 23:31:26 -0500

    Reading in json

--- Pathfinding/main.cpp
+++ Pathfinding/main.cpp
@@ -1,10 +1,28 @@
 #include <stdio.h>
+#include <fstream>
+#include <string>
 
 int main(int argc, const char* argv[]) {
 	if (argc != 3) {
 		printf("Please run this program again, specifying an input file and an output file.");
+		system("pause");
 		return 0;
 	}
-	printf("The input file is %s and the output file is %s", argv[1], argv[2]);
+	printf("The input file is %s and the output file is %s \n", argv[1], argv[2]);
+	std::ifstream input;
+	input.open(argv[1]);
+	if (!input) {
+		printf("The file %s does not exist.", argv[1]);
+	}
+	std::string contents = "";
+	std::string line;
+	while (input >> line) {
+		contents = contents + line + "\n";
+	}
+
+	printf(contents.c_str());
+
+	system("pause");
+
 	return 0;
 }
